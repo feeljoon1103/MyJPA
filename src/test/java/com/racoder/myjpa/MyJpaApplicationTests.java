ef7c3ed5e8a2c9ad7bd34009efa0f7b83f5fbead
Joon <feeljoon1103@gmail.com>
2021-12-14 15:36:48 +0900

    init

---
+++ src/test/java/com/racoder/myjpa/MyJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.racoder.myjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
